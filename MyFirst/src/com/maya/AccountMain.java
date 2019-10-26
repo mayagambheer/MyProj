95e278c9ddae008194ba65108c50eedaf42acbe7
maya.gambheer <maya.gambheer@WKSBAN24KAN0001.corp.mphasis.com>
2019-10-26 09:59:18 +0530

    main class commit

---
+++ MyFirst/src/com/maya/AccountMain.java
@@ -0,0 +1,17 @@
+package com.maya;
+
+public class AccountMain {
+	
+	public static void main(String args[])
+	{
+	Account a = new Account(1,"sbi");
+	System.out.println(a);
+	
+	
+	}
+	
+	
+	
+	//main class
+	
+}
